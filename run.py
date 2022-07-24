a853e10e879d73af0b3bba9b250b8849251d76af
gfpkelly1986 <gfpkelly@gmail.com>
2022-07-24 11:08:51 +0000

    Set up initial functions and test their responses.

--- run.py
+++ run.py
@@ -16,6 +16,32 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('SurveyResponseForm')
 
-form_responses = SHEET.worksheet('Form Responses').get_all_values()
 
-print(form_responses)
+def begin_program():
+    """
+    This function will run, welcome the user and take a response from them
+    If they want to see their form responses it will call check_response_data
+    If not it will close the program
+    """
+    print('This is a test')
+
+
+def check_response_data():
+    """
+    Access the form responses and count the number of rows
+    """
+    all_values = SHEET.worksheet('Form Responses').get_all_values()
+    row_count = int(len(all_values)) - 1
+    print(row_count)
+    return row_count
+
+
+def main():
+    """
+    This is the main function that controls the general flow of the program
+    """
+    begin_program()
+    check_response_data()
+
+
+main()
